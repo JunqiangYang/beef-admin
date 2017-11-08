60becd4fd7b3983aafb59e26c47550f37c208f2d
JunqiangYang <junqiang.yang.it@gmail.com>
2017-11-08 15:17:51 +0800

    创建进库记录

--- web/js/commons/urls.js
+++ web/js/commons/urls.js
@@ -5,7 +5,7 @@ var urls= {
 	'easyuiUrl':'http://127.0.0.1:8080/ms/js/jquery-easyui-1.3.2'
 }*/
 var urls= {
-	'msUrl':'/jeecg-mybatis-framework/',
+	'msUrl':'',
 	'easyuiUrl':'/js/jquery-easyui-1.3.2'
 }
 //alert(urls['msUrl']);
