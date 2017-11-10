645469a9beb269eb6af4460e163bb12bc026a364
JunqiangYang <junqiang.yang.it@gmail.com>
2017-11-10 13:36:07 +0800

    add out

---
+++ web/view/scott/outgoods/page-tRecords.js
@@ -0,0 +1,362 @@
+$package('jeecg.tRecords');
+jeecg.tRecords = function(){
+	var _box = null;
+
+	// 缓存用户,型号,仓库 信息
+	var personlist = null ;
+	var goodskindlist = null ;
+	var warehouselist = null ;
+
+	var appendstr = function (str , append) {
+        if(str.trim().length == 0){
+            return append+"," ;
+        }
+        if(str.endsWith(",")){
+            return str+append+"," ;
+        }
+        if(!str.endsWith(",")){
+            return str+","+append+"," ;
+        }
+    }
+
+    var sum = function (str) {
+        var total = 0 ;
+        str+="";
+        var arr =   str.split(",");
+        //for (let obj of arr) {
+            for (var i=0; i<arr.length  ; i++){
+                var obj = arr[i] ;
+            if(obj.trim().length != 0 ){
+                total += parseInt(obj);
+            }
+        }
+        return total;
+    }
+
+    var formatWeight =function (value){
+        if(value=="" || value==undefined){
+            return ""
+        }
+        value+="";
+        var len = value.length ;
+        return value.toString().substring(0,len-3)+"."+value.toString().substring(len-3);
+    }
+//---------------
+    var refreshtotal = function(){
+        var deatils = '' ;
+        var inputs = $("input[name^='numinput']") ;
+        $(inputs).each(function (i,n) {
+            var str = $(this).val() ;
+            var weightformat = $("#weightformat").val() ;
+            var regex = /^\d{5}$/ ;
+            if(parseInt(weightformat) == 1){
+                regex =/^\d{4}$/;
+            }
+            if(regex.test(str) || str.trim().length == 0 ){
+                deatils = appendstr(deatils,str) ;
+            }
+        })
+        $("#totalweight").html(sum(deatils)) ;
+    }
+
+    // 创建
+	var new_4_5_div = function (str) {
+	    //var str = $('#details').val();
+        $("#details").val('');
+	    var arr = new Array(100);
+        for(var j = 0,len=arr.length; j<len; j++) { arr[j] = ''; }
+	    if(str != null && str.trim().length != 0 ){
+            var a = str.split(",");
+            for(var j = 0,len=a.length; j<len; j++) { arr[j] = a[j]; }
+        }
+        console.info(arr);
+        var n = 0 ;
+        var html = '' ;
+        for (var i=0;i<4;i++)
+        {
+            for (var j=0;j<5;j++)
+            {
+                html += '<div class="blockdiv">' ;
+                html += '<input name="numinput" type="text" value="'+(arr[n++])+'" >';
+                html += '<input name="numinput" type="text" value="'+(arr[n++])+'" >';
+                html += '<input name="numinput" type="text" value="'+(arr[n++])+'" >';
+                html += '<input name="numinput" type="text" value="'+(arr[n++])+'" >';
+                html += '<input name="numinput" type="text" value="'+(arr[n++])+'" >';
+                html += '</div>';
+            }
+        }
+        $('#divlistblock').html(html);
+        $("div[name=divnuminputblock]").addClass("blockdiv");
+        $("input[name='numinput']").addClass("blockdiv-input");
+        var inputs = $("input[name^='numinput']") ;
+        var len = inputs.length ;
+        $(inputs).each(function (i,n) {
+            $(this).keyup(function(e) {
+                var str = $(this).val() ;
+                var weightformat = $("#weightformat").val() ;
+                var regex = /^\d{5}$/ ;
+                if(parseInt(weightformat) == 1){
+                    regex =/^\d{4}$/;
+                }
+                if(regex.test(str) || str.trim().length ==0){
+                    $(this).removeClass("redborder");
+                    if(i<len-1){
+                       $(inputs[i+1]).focus();
+                    }
+                    refreshtotal();
+                }else{
+                    $(this).addClass("redborder");
+                }
+            });
+        });
+    };
+
+
+
+    var new_1_1_div = function (str) {
+        //var str = $('#details').val();
+        $("#details").val('');
+        var arr = new Array(100);
+        for(var j = 0,len=arr.length; j<len; j++) { arr[j] = ''; }
+        if(str != null && str.trim().length != 0 ){
+            var a = str.split(",");
+            for(var j = 0,len=a.length; j<len; j++) { arr[j] = a[j]; }
+        }
+        console.info(arr);
+        var n = 0 ;
+        var html = '' ;
+        html += '<div class="blockdiv">' ;
+        html += '<input name="numinput" type="text" value="'+(arr[n++])+'" >';
+        html += '</div>';
+        $('#divlistblock').html(html);
+        $("div[name=divnuminputblock]").addClass("blockdiv");
+        $("input[name='numinput']").addClass("blockdiv-input");
+        var inputs = $("input[name^='numinput']") ;
+        var len = inputs.length ;
+        $(inputs).each(function (i,n) {
+            $(this).keyup(function(e) {
+                var str = $(this).val() ;
+                var weightformat = $("#weightformat").val() ;
+                var regex = /^\d{5}$/ ;
+                if(parseInt(weightformat) == 1){
+                    regex =/^\d{4}$/;
+                }
+                if(regex.test(str) || str.trim().length ==0){
+                    $(this).removeClass("redborder");
+                    if(i<len-1){
+                        $(inputs[i+1]).focus();
+                    }
+                    refreshtotal();
+                }else{
+                    $(this).addClass("redborder");
+                }
+            });
+        });
+    };
+
+    var _this = {
+		config:{
+			event:{
+				add:function(){
+                    _box.handler.add();
+                    $('#formpeopleid').combobox({
+                        data:personlist,
+                        valueField:'id',
+                        textField:'name'
+                    });
+                    $('#formgoodskindid').combobox({
+                        data:goodskindlist,
+                        valueField:'id',
+                        textField:'gooddskindname'
+                    });
+                    $('#formwarehouseid').combobox({
+                        data:warehouselist,
+                        valueField:'id',
+                        textField:'warehousename'
+                    });
+                    // 创建4行5列 矩阵框
+                    //new_4_5_div('');
+                    $("#totalweight").html();
+
+				},
+				edit:function(){
+                    $('#formpeopleid').combobox({
+                        data:personlist,
+                        valueField:'id',
+                        textField:'name'
+                    });
+                    $('#formgoodskindid').combobox({
+                        data:goodskindlist,
+                        valueField:'id',
+                        textField:'gooddskindname'
+                    });
+                    $('#formwarehouseid').combobox({
+                        data:warehouselist,
+                        valueField:'id',
+                        textField:'warehousename'
+                    });
+					_box.handler.edit(function (result) {
+                        $("#totalweight").html();
+                        $("#remarktextarea").val($("#remark").val());
+                        var isfixedweight  = parseInt($('#formgoodskindid').combobox('getValue'));
+                        if(result.data.isfixedweight != undefined && parseInt(result.data.id) == parseInt(n) && result.data.isfixedweight ==0 ){
+                            new_1_1_div(result.data.details);
+                        }else{
+                            // 创建4行5列 矩阵框
+                            new_4_5_div(result.data.details);
+                        }
+                        var total =  sum(result.data.details)
+                        $("#totalweight").html(total)  ;
+                    });
+				},
+                save:function () {
+                    var check = true ;
+				    var deatils = '' ;
+
+                    $("input[name='numinput']").each(function () {
+                        var str = $(this).val() ;
+                        var weightformat = $("#weightformat").val() ;
+                        var regex = /^\d{5}$/ ;
+                        if(parseInt(weightformat) == 1){
+                            regex =/^\d{4}$/;
+                        }
+                        if(regex.test(str) || str.trim().length ==0){
+                            $(this).removeClass("redborder");
+                            if(str.trim().length>0) {deatils += (str+",") ; }
+                        }else{
+                            $(this).addClass("redborder");
+                            check = false ;
+                        }
+                    });
+                    if(!check) return;
+				    console.info(deatils);
+                    var total =  sum(deatils)
+                    var showtotal = parseInt($("#totalweight").html()) ;
+                    if(showtotal != total){
+                        jeecg.alert('提示',' 表单总数计算与显示异常','error');
+                        return ;
+                    }
+                    if(deatils.length>0) deatils=deatils.substring(0,deatils.length-1);
+                    $("#details").val(deatils);
+                    $("#remark").val($("#remarktextarea").val());
+                    _box.handler.save();
+                }
+			},
+  			dataGrid:{
+  				title:'待打印_出库_记录管理',
+	   			url:'dataList.do',
+	   			columns:[[
+					{field:'id',checkbox:true},
+                    {field:'createtime',title:'创建时间',align:'center',sortable:true,
+                        formatter:function(value,row,index){
+                            return row.createtime;
+                        }
+                    },
+                    {field:'name',title:'买家',align:'center',sortable:true,
+                        formatter:function(value,row,index){
+                            return row.name;
+                        }
+                    },
+                    {field:'gooddsKindName',title:'型号',align:'center',sortable:true,
+                        formatter:function(value,row,index){
+                            return row.gooddsKindName;
+                        }
+                    },
+                    {field:'nums',title:'件数',align:'center',sortable:true,
+                        formatter:function(value,row,index){
+                            return row.nums;
+                        }
+                    },
+                    {field:'price',title:'价格',align:'center',sortable:true,
+                        formatter:function(value,row,index){
+                            return row.price;
+                        }
+                    },
+                    {field:'weightotal',title:'总重',align:'center',sortable:true,
+                        formatter:function(value,row,index){
+                            return formatWeight(sum(row.details)) ;
+                        }
+                    },
+					{field:'warehouseName',title:'仓库',align:'center',sortable:true,
+							formatter:function(value,row,index){
+								return row.warehouseName;
+							}
+						},
+					{field:'status',title:'操作',align:'center',sortable:true,
+							formatter:function(value,row,index){
+								return "<a href=''></a>";
+							}
+						}
+					]]
+			}
+		},
+		init:function(){
+			_box = new YDataGrid(_this.config); 
+			_box.init();
+
+			jeecg.progress();
+            jeecg.ajaxJson(urls['msUrl']+'/tContactPerson/dataList.do',{},function(data){
+                if(data.total != null && data.total > 0 ){
+                    personlist = [{id:-99,name:"--请选择--","selected":true}].concat(data.rows) ;
+                    $('#peopleid').combobox({
+                        data:personlist,
+                        valueField:'id',
+                        textField:'name'
+                    });
+                }else{
+                    jeecg.alert('提示',data.msg,'error');
+                }
+                jeecg.closeProgress();
+            });
+            jeecg.progress();
+            jeecg.ajaxJson(urls['msUrl']+'/tGoodsKind/dataList.do',{},function(data){
+                if(data.total != null && data.total > 0 ){
+                    goodskindlist =  [{id:-99,gooddskindname:"--请选择--","selected":true}].concat(data.rows)
+                    $('#goodskindid').combobox({
+                        data:goodskindlist,
+                        valueField:'id',
+                        textField:'gooddskindname'
+                    });
+                }else{
+                    jeecg.alert('提示',data.msg,'error');
+                }
+                jeecg.closeProgress();
+            });
+
+            jeecg.progress();
+            jeecg.ajaxJson(urls['msUrl']+'/tWarehouse/dataList.do',{},function(data){
+                if(data.total != null && data.total > 0 ){
+                    warehouselist = [{id:-99,warehousename:"--请选择--","selected":true}].concat(data.rows)
+                    $('#warehouseid').combobox({
+                        data:warehouselist,
+                        valueField:'id',
+                        textField:'warehousename'
+                    });
+                }else{
+                    jeecg.alert('提示',data.msg,'error');
+                }
+                jeecg.closeProgress();
+            });
+            $("#formgoodskindid").combobox({
+                onChange: function (n,o) {
+                    console.info(n);
+                   // for (let obj of goodskindlist) {
+                        for (var i=0; i<goodskindlist.length  ; i++){
+                            var obj = goodskindlist[i] ;
+                        if(obj.isfixedweight != undefined && parseInt(obj.id) == parseInt(n) && obj.isfixedweight ==0 ){
+                            new_4_5_div('');
+                            return ;
+                        }
+                    }
+                    new_1_1_div('');
+                    $("#totalweight").html();
+                }
+            });
+		}
+	}
+	return _this;
+}();
+
+$(function(){
+	jeecg.tRecords.init();
+});
---
+++ web/view/scott/outgoods/tRecords.jsp
@@ -0,0 +1,102 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+   <%@include file="/view/resource.jsp" %>
+  </head>
+  <style>
+	  .blockdiv{
+		  width: 60px;padding-top: 5px; margin-left: 5px; margin-right: 5px;float:left;text-align: center ;
+	  }
+	  .blockdiv-input{
+		  width: 100%;border: 1px solid black;padding-top: 3px;margin: 1px;
+	  }
+	  .redborder{
+		  border: 1px solid red;
+	  }
+
+
+  </style>
+  <body class="easyui-layout">
+ 	 <!-- Search panel start -->
+ 	 <div class="ui-search-panel" region="north" style="height: 80px;" title="过滤条件" data-options="striped: true,collapsible:false,iconCls:'icon-search',border:false" >  
+ 	 <form id="searchForm">
+        <p class="ui-fields">
+			<label class="ui-label">仓库:</label>
+			<select  class="easyui-combobox"   name="warehouseid" id="warehouseid"  style="width:100px;">
+			</select>
+			<label class="ui-label">型号:</label>
+			<select  class="easyui-combobox"  name="goodskindid" id="goodskindid" style="width:100px;" >
+			</select>
+			<label class="ui-label">买家:</label>
+			<select  class="easyui-combobox"   name="peopleid" id="peopleid" style="width:100px;" >
+			</select>
+			<label class="ui-label">时间阶段:</label>
+			<input name="begintime" id="begintime" type="text" maxlength="" class="easyui-datetimebox" >
+			-
+			<input name="endtime" id="endtime" type="text" maxlength="" class="easyui-datetimebox" >
+
+
+	    </p>
+	    <a href="#" id="btn-search" class="easyui-linkbutton" iconCls="icon-search">查询</a>
+      </form>  
+     </div> 
+     <!--  Search panel end -->
+
+     <!-- Data List -->
+     <div region="center" border="false" >
+     <table id="data-list"></table>
+	 </div>
+	 
+     <!-- Edit Win&Form -->
+     <div id="edit-win" class="easyui-dialog" title="出库码单" data-options="closed:true,iconCls:'icon-save',modal:true" style=" margin-top: 5px; width:470px; height: 600px;">
+     	<form id="editForm" class="ui-form" method="post">  
+     		 <input class="hidden" name="id" >
+     		 <div class="ui-edit">
+		     	   <div class="ftitle"></div>
+					<div>
+						<label>仓库:</label>
+						<select class="easyui-combobox" name="warehouseid" id="formwarehouseid" style="width: 80px"></select>
+						<label>时间:</label>
+						<input name="createtime" type="text" maxlength="" class="easyui-datetimebox" missingMessage="请填写创建时间">
+						<br/>
+						<label>买家:</label>
+						<select class="easyui-combobox" name="peopleid" id="formpeopleid" style="width: 80px">
+						</select>
+						<label>型号:</label>
+						<select class="easyui-combobox" name="goodskindid" id="formgoodskindid" style="width: 80px"></select>
+						<label>价格:</label>
+						<input name="price" type="text" class="easyui-textbox"  data-options="required:true" missingMessage="请填价格" style="width: 55px">
+						<label>件数:</label>
+						<input name="nums" type="text" maxlength="" class="easyui-numberbox"  data-options="required:true" missingMessage="请填写件数" style="width: 55px">
+
+					<%--<label>状态</label>--%>
+						<input name="status" type="hidden" value="0" >
+					</div>
+
+					<div style="margin-top: 10px; padding-top: 5px;  border-top:1px solid #ccc; ">
+						<label>重量格式:</label>
+							<select id="weightformat" name="weightformat" style="width: 80px">
+								<option value="0">五位数</option>
+								<option value="1">四位数</option>
+							</select>
+						<br/><br/>
+						<input name="details" id="details" type="hidden" >
+						<div id="divlistblock" style=" width: 88%; text-align: center; margin: 0 auto; "></div>
+						<div style="clear: both;margin-top: 5px">
+							<%--<label>备注:</label>--%>
+							<input name="remark" id="remark" type="hidden" >
+							<%--<textarea id="remarktextarea" style="width: 100%;height: 50px " ></textarea>--%>
+								&nbsp;&nbsp;
+							<label>总重:</label>
+							<label id="totalweight" >******</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<label>总价:</label>
+							<label id="totalprice">******</label>
+						</div>
+					</div>
+  			</div>
+     	</form>
+  	 </div>
+  	 <script type="text/javascript" src="<%=basePath%>/view/scott/outgoods/page-tRecords.js"></script>
+  </body>
+</html>
