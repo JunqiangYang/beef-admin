e2b955b0bcb916119e3911a17f3db0ad0c00992a
JunqiangYang <junqiang.yang.it@gmail.com>
2017-11-10 15:24:25 +0800

    mapper  fixed

--- web/view/scott/t_records/page-tRecords.js
+++ web/view/scott/t_records/page-tRecords.js
@@ -61,74 +61,6 @@ jeecg.tRecords = function(){
     var _this = {
 		config:{
 			event:{
-				add:function(){
-                    _box.handler.add();
-                    $('#formpeopleid').combobox({
-                        data:personlist,
-                        valueField:'id',
-                        textField:'name'
-                    });
-                    $('#formgoodskindid').combobox({
-                        data:goodskindlist,
-                        valueField:'id',
-                        textField:'gooddskindname'
-                    });
-                    $('#formwarehouseid').combobox({
-                        data:warehouselist,
-                        valueField:'id',
-                        textField:'warehousename'
-                    });
-                    // 创建4行5列 矩阵框
-                    new_4_5_div('');
-
-				},
-				edit:function(){
-                    $('#formpeopleid').combobox({
-                        data:personlist,
-                        valueField:'id',
-                        textField:'name'
-                    });
-                    $('#formgoodskindid').combobox({
-                        data:goodskindlist,
-                        valueField:'id',
-                        textField:'gooddskindname'
-                    });
-                    $('#formwarehouseid').combobox({
-                        data:warehouselist,
-                        valueField:'id',
-                        textField:'warehousename'
-                    });
-					_box.handler.edit(function (result) {
-                        $("#remarktextarea").val($("#remark").val());
-                        // 创建4行5列 矩阵框
-                        new_4_5_div(result.data.details);
-                    });
-				},
-                save:function () {
-                    var check = true ;
-				    var deatils = '' ;
-                    $("input[name='numinput']").each(function () {
-                        var str = $(this).val() ;
-                        var weightformat = $("#weightformat").val() ;
-                        var regex = /^\d{5}$/ ;
-                        if(parseInt(weightformat) == 1){
-                            regex =/^\d{4}$/;
-                        }
-                        if(regex.test(str) || str.trim().length ==0){
-                            $(this).removeClass("redborder");
-                            if(str.trim().length>0) deatils += (str+",") ;
-                        }else{
-                            $(this).addClass("redborder");
-                            check = false ;
-                        }
-                    });
-                    if(!check) return;
-				    console.info(deatils);
-                    if(deatils.length>0) deatils=deatils.substring(0,deatils.length-1);
-                    $("#details").val(deatils);
-                    $("#remark").val($("#remarktextarea").val());
-                    _box.handler.save();
-                }
 			},
   			dataGrid:{
   				title:'进出库记录管理',
@@ -164,12 +96,8 @@ jeecg.tRecords = function(){
 							formatter:function(value,row,index){
 								return row.nums;
 							}
-						},
-					{field:'name',title:'买家',align:'center',sortable:true,
-							formatter:function(value,row,index){
-								return row.name;
-							}
 						}
+
 					// {field:'status',title:'状态',align:'center',sortable:true,
 					// 		formatter:function(value,row,index){
 					// 			return row.status;
@@ -193,50 +121,6 @@ jeecg.tRecords = function(){
 		init:function(){
 			_box = new YDataGrid(_this.config); 
 			_box.init();
-
-			jeecg.progress();
-            jeecg.ajaxJson(urls['msUrl']+'/tContactPerson/dataList.do',{},function(data){
-                if(data.total != null && data.total > 0 ){
-                    personlist = [{id:-99,name:"--请选择--","selected":true}].concat(data.rows) ;
-                    $('#peopleid').combobox({
-                        data:personlist,
-                        valueField:'id',
-                        textField:'name'
-                    });
-                }else{
-                    jeecg.alert('提示',data.msg,'error');
-                }
-                jeecg.closeProgress();
-            });
-            jeecg.progress();
-            jeecg.ajaxJson(urls['msUrl']+'/tGoodsKind/dataList.do',{},function(data){
-                if(data.total != null && data.total > 0 ){
-                    goodskindlist =  [{id:-99,gooddskindname:"--请选择--","selected":true}].concat(data.rows)
-                    $('#goodskindid').combobox({
-                        data:goodskindlist,
-                        valueField:'id',
-                        textField:'gooddskindname'
-                    });
-                }else{
-                    jeecg.alert('提示',data.msg,'error');
-                }
-                jeecg.closeProgress();
-            });
-
-            jeecg.progress();
-            jeecg.ajaxJson(urls['msUrl']+'/tWarehouse/dataList.do',{},function(data){
-                if(data.total != null && data.total > 0 ){
-                    warehouselist = [{id:-99,warehousename:"--请选择--","selected":true}].concat(data.rows)
-                    $('#warehouseid').combobox({
-                        data:warehouselist,
-                        valueField:'id',
-                        textField:'warehousename'
-                    });
-                }else{
-                    jeecg.alert('提示',data.msg,'error');
-                }
-                jeecg.closeProgress();
-            });
 
 		}
 	}
